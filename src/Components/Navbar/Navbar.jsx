158069173338d56b869b51654c1badfceb7d0507
Samah <samehrizk1993@gmail.com>
2026-02-16 01:36:44 +0200

    Add new changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
 
   const textColor = theme === "dark" ? "text-white" : "text-gray-700";
 
-  const sidebarBg = theme === "dark" ? "bg-[#1a1a1a]" : "bg-white";
+  const sidebarBg = theme === "dark" ? "bg-[#101010]" : "bg-white";
 
   return (
     <div
